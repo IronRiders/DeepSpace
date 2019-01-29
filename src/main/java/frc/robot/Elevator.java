2c19d5c85e9755efe49633b1d65e059b515904e8
Iron Riders <Corinneh2001@gmail.com>
2019-01-29 15:58:22 -0800

    wrote elevator code with talonsrx position mode -Kevin + Corinne

---
+++ src/main/java/frc/robot/Elevator.java
@@ -0,0 +1,37 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class Elevator {
+    private TalonSRX talon;
+    private final double pConstant = 0.3;
+    private final double iConstant = 0.3;
+    private final double dConstant = 0.3;
+    private final double fConstant = 0.0;
+
+    public Elevator(int elevatorPort){
+        talon = new TalonSRX(elevatorPort);
+        talon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+        talon.config_kD(0, dConstant);
+        talon.config_kP(0, pConstant);
+        talon.config_kI(0, iConstant);
+        talon.config_kF(0, fConstant);
+    }
+
+    public void move(double distance){
+        double totalPulses = distance * (4096/(Math.PI*6));
+        talon.set(ControlMode.Position, totalPulses);
+    }
+
+
+
+
+
+
+
+
+
+
+}
--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -7,7 +7,9 @@ public class Ports{
     public static final int LEFT_DRIVETRAIN_2 = 4;
     public static final int RIGHT_DRIVETAIN_1 = 2;
     public static final int RIGHT_DRIVETAIN_2 = 3;
-    public static final int GYRO_PORT = 5;
+    public static final int GYRO_PORT = 0;
+    public static final int ELEVATOR_PORT = 6; //needs to be set
+
     
 
 }
