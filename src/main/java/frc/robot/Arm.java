eaf56027b3800a945d4e93fbe2dca33a766f8e56
Iron Riders <Corinneh2001@gmail.com>
2019-01-30 16:35:38 -0800

    started working on arm -Ryan

---
+++ src/main/java/frc/robot/Arm.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+ 
+public class Arm{
+    private CANSparkMax spark;
+    private CANEncoder encoder;
+    private CANPIDController pid;
+    private int distance;
+   // double pulsesPerInch = 4096/(Math.PI*diameter of wheel)
+
+    public Arm(int portNumber){
+        spark = new CANSparkMax(portNumber, MotorType.kBrushless);
+        encoder = spark.getEncoder();
+        pid = spark.getPIDController();
+    }
+
+
+}
