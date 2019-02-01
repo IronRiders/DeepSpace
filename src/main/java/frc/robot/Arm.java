cb8994eb9c11eaddce6d1fe35e3f8f6d1b90363c
Alex Maclean <Corinneh2001@gmail.com>
2019-01-31 18:54:18 -0800

    arm fixed, finished the elevator

--- src/main/java/frc/robot/Arm.java
+++ src/main/java/frc/robot/Arm.java
@@ -3,20 +3,37 @@ package frc.robot;
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
  
 public class Arm{
     private CANSparkMax spark;
     private CANEncoder encoder;
     private CANPIDController pid;
-    private int numRevolutions;
+    private int numRevolutions = 5; //ask bailey
+    private final double pConstant = 0.3;
+    private final double iConstant = 0.01;
+    private final double dConstant = 4.0;
+    private final double fConstant = 0.0;
+    private final int maxMotorAmps = 5;
    // double pulsesPerInch = 4096/(Math.PI*diameter of wheel)
 
     public Arm(int portNumber){
         spark = new CANSparkMax(portNumber, MotorType.kBrushless);
         encoder = spark.getEncoder();
         pid = spark.getPIDController();
+        pid.setD(dConstant);
+        pid.setP(pConstant);
+        pid.setI(dConstant);
+        pid.setFF(fConstant);
+        spark.setSecondaryCurrentLimit(maxMotorAmps);
+
+
     }
 
+    public void makeItFastOrSlow(){
+        pid.setReference(numRevolutions, ControlType.kPosition);
 
-}
+    }
+        
+    }
