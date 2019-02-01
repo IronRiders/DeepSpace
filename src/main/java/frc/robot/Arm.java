b5d40a18cc23765cd1a46b6b985d5e4a6d3bf5a3
Alex Maclean <Corinneh2001@gmail.com>
2019-01-31 19:13:09 -0800

    Set a limit switch for arm.

--- src/main/java/frc/robot/Arm.java
+++ src/main/java/frc/robot/Arm.java
@@ -5,6 +5,7 @@ import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.DigitalInput;
  
 public class Arm{
     private CANSparkMax spark;
@@ -16,9 +17,10 @@ public class Arm{
     private final double dConstant = 4.0;
     private final double fConstant = 0.0;
     private final int maxMotorAmps = 5;
+    DigitalInput limitSwitch;
    // double pulsesPerInch = 4096/(Math.PI*diameter of wheel)
 
-    public Arm(int portNumber){
+    public Arm(int portNumber, int limitSwitchPort){
         spark = new CANSparkMax(portNumber, MotorType.kBrushless);
         encoder = spark.getEncoder();
         pid = spark.getPIDController();
@@ -27,6 +29,7 @@ public class Arm{
         pid.setI(dConstant);
         pid.setFF(fConstant);
         spark.setSecondaryCurrentLimit(maxMotorAmps);
+        limitSwitch = new DigitalInput(limitSwitchPort);
 
 
     }
@@ -35,5 +38,12 @@ public class Arm{
         pid.setReference(numRevolutions, ControlType.kPosition);
 
     }
-        
+    public void pushySwitch(){
+        // 1 is true(open) , zero is false(closed)
+        if (limitSwitch.get()) {
+            spark.set( -0.3);
+        }   
+        else {
+            spark.set(0);
+        }
     }
