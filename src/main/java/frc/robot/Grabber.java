638a74823aefc1524c003d298c6136b52246e380
Alex Maclean <Corinneh2001@gmail.com>
2019-02-07 19:10:33 -0800

    finished up manipulator with grabber

--- src/main/java/frc/robot/Grabber.java
+++ src/main/java/frc/robot/Grabber.java
@@ -1,16 +1,41 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.VictorSP;
 
 public class Grabber {
     private VictorSP leftFlywheel;
     private VictorSP rightFlywheel;
+    private TalonSRX rightClaw;
+    private TalonSRX leftClaw;
+    private DigitalInput leftLimitSwitch;
+    private DigitalInput rightLimitSwitch;
     private final double flywheelSpeed = 0.5;
+    private final double pConstant = 0.6;
+    private final double iConstant = 10;
+    private final double dConstant = 0.0;
+    private final double fConstant = 0.0;
+    private final int maxAmps = 10;
 
-    public Grabber(int leftFlywheelPort , int rightFlywheelPort){
+
+    public Grabber(int leftFlywheelPort , int rightFlywheelPort , int rightClawPort , int leftClawPort){
         leftFlywheel = new VictorSP(leftFlywheelPort);
         rightFlywheel = new VictorSP(rightFlywheelPort);
+        rightClaw = new TalonSRX(rightClawPort);
+        leftClaw = new TalonSRX(leftClawPort);
+        rightClaw.config_kD(0, dConstant);
+        rightClaw.config_kP(0, pConstant);
+        rightClaw.config_kI(0, iConstant);
+        rightClaw.config_kF(0, fConstant);
+        rightClaw.configPeakCurrentLimit(maxAmps);
 
+        leftClaw.config_kD(0, dConstant);
+        leftClaw.config_kP(0, pConstant);
+        leftClaw.config_kI(0, iConstant);
+        leftClaw.config_kF(0, fConstant);
+        leftClaw.configPeakCurrentLimit(maxAmps);
     }
 
     public void intake(){
