b2b22e09d2a7184ac8e31ca1b97464ff461d8a2c
Alex Fischman <alex@fischman.org>
2019-10-14 21:18:17 -0700

    Converted CargoPusher to use a regular Solenoid

--- src/main/java/frc/robot/CargoPusher.java
+++ src/main/java/frc/robot/CargoPusher.java
@@ -1,24 +1,19 @@
-//package frc.team4180;// why is this diffrent?
 package frc.robot;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class CargoPusher {
-    public boolean BoxTipped;
-    private final DoubleSolenoid upperPiston;
-    //For now cargoPusher handles both cargo and hatches lille debugging. 
-    //To run just hatches remove code for ports 4-6
+    private final Solenoid piston;
 
-    CargoPusher(final int port1, final int port2) {
-        upperPiston = new DoubleSolenoid(port1, port2);
+    CargoPusher(final int port) {
+        piston = new Solenoid(port);
     }
 
     public void drop() {
-        upperPiston.set(DoubleSolenoid.Value.kForward);
-        //BoxTipped=true;
+        piston.set(true);
     }
 
     public void lock() {
-        upperPiston.set(DoubleSolenoid.Value.kReverse);
+        piston.set(false);
     }
 }
